0e866e4c9616cc6eacdf88387adff8a10d73ccdb
juana <juanitaestupinan2@gmail.com>
2023-03-10 20:52:28 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+// escriba la funcion hola aca
+function hola(nombre) {
+    return "Hola " + nombre + "!"
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
